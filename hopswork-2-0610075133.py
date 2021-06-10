952b98522959f994fc4c9ebc1d9e4fe39084f5f5
sleepo-chin <56860530+sleepo-chin@users.noreply.github.com>
2021-06-10 14:51:35 +0700

    Pushed DAG hopswork-2-0610075133

---
+++ hopswork-2-0610075133.py
@@ -0,0 +1,108 @@
+from airflow import DAG
+
+from airflow_notebook.pipeline import NotebookOp
+from airflow.utils.dates import days_ago
+
+# Setup default args with older date to automatically trigger when uploaded
+args = {
+    "project_id": "hopswork-2-0610075133",
+}
+
+dag = DAG(
+    "hopswork-2-0610075133",
+    default_args=args,
+    schedule_interval="@once",
+    start_date=days_ago(1),
+    description="Created with Elyra 2.3.0.dev0 pipeline editor using `telco-etl-test.pipeline`.",
+    is_paused_upon_creation=False,
+)
+
+
+notebook_op_15c8ef03_d6f7_4c41_a682_11676d9b1f85 = NotebookOp(
+    name="Extract_datasource",
+    namespace="airflow",
+    task_id="Extract_datasource",
+    notebook="demo-pipeline/Extract datasource.ipynb",
+    cos_endpoint="http://minio-kubeflow.apps.ocp.ikp.com/",
+    cos_bucket="test",
+    cos_directory="hopswork-2-0610075133",
+    cos_dependencies_archive="Extract datasource-15c8ef03-d6f7-4c41-a682-11676d9b1f85.tar.gz",
+    pipeline_outputs=["test/telco-data.csv"],
+    pipeline_inputs=[],
+    image="amancevice/pandas:1.1.1",
+    in_cluster=True,
+    env_vars={
+        "AWS_ACCESS_KEY_ID": "minio",
+        "AWS_SECRET_ACCESS_KEY": "minio123",
+        "ELYRA_ENABLE_PIPELINE_INFO": "True",
+        "DIR_NAME": "test",
+        "DIR_FILE_NAME": "telco-data.csv",
+    },
+    config_file="None",
+    dag=dag,
+)
+
+notebook_op_15c8ef03_d6f7_4c41_a682_11676d9b1f85.image_pull_policy = "IfNotPresent"
+
+
+notebook_op_9e3fe02a_9694_429d_9633_cc62bdf4c572 = NotebookOp(
+    name="transform",
+    namespace="airflow",
+    task_id="transform",
+    notebook="demo-pipeline/transform.ipynb",
+    cos_endpoint="http://minio-kubeflow.apps.ocp.ikp.com/",
+    cos_bucket="test",
+    cos_directory="hopswork-2-0610075133",
+    cos_dependencies_archive="transform-9e3fe02a-9694-429d-9633-cc62bdf4c572.tar.gz",
+    pipeline_outputs=["test/telco_data.csv"],
+    pipeline_inputs=["test/telco-data.csv"],
+    image="amancevice/pandas:1.1.1",
+    in_cluster=True,
+    env_vars={
+        "AWS_ACCESS_KEY_ID": "minio",
+        "AWS_SECRET_ACCESS_KEY": "minio123",
+        "ELYRA_ENABLE_PIPELINE_INFO": "True",
+        "MINIO_ACCESS_KEY": "minio",
+        "MINIO_SECRET_KEY": "minio123",
+        "MINIO_BUCKET": "data-telco",
+    },
+    config_file="None",
+    dag=dag,
+)
+
+notebook_op_9e3fe02a_9694_429d_9633_cc62bdf4c572.image_pull_policy = "IfNotPresent"
+
+(
+    notebook_op_9e3fe02a_9694_429d_9633_cc62bdf4c572
+    << notebook_op_15c8ef03_d6f7_4c41_a682_11676d9b1f85
+)
+
+
+notebook_op_225cd238_97e0_4418_8183_734355388b5d = NotebookOp(
+    name="load_data_to_hw",
+    namespace="airflow",
+    task_id="load_data_to_hw",
+    notebook="demo-pipeline/load data to hw.ipynb",
+    cos_endpoint="http://minio-kubeflow.apps.ocp.ikp.com/",
+    cos_bucket="test",
+    cos_directory="hopswork-2-0610075133",
+    cos_dependencies_archive="load data to hw-225cd238-97e0-4418-8183-734355388b5d.tar.gz",
+    pipeline_outputs=[],
+    pipeline_inputs=["test/telco_data.csv", "test/telco-data.csv"],
+    image="amancevice/pandas:1.1.1",
+    in_cluster=True,
+    env_vars={
+        "AWS_ACCESS_KEY_ID": "minio",
+        "AWS_SECRET_ACCESS_KEY": "minio123",
+        "ELYRA_ENABLE_PIPELINE_INFO": "True",
+    },
+    config_file="None",
+    dag=dag,
+)
+
+notebook_op_225cd238_97e0_4418_8183_734355388b5d.image_pull_policy = "IfNotPresent"
+
+(
+    notebook_op_225cd238_97e0_4418_8183_734355388b5d
+    << notebook_op_9e3fe02a_9694_429d_9633_cc62bdf4c572
+)
